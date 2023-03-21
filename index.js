3ac306b63cc37994414d17657a078cbf4b357832
Riley <riley.freier@gmail.com>
2023-03-21 16:50:41 -0600

    Functions function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork (activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(parameter = "*") {
+	return function(wrap = "special") {
+		return `You are ${parameter}${wrap}${parameter}!`;
+	};
+}
+
+let result = wrapAdjective("*", "a hard worker");
+console.log(result()); 
